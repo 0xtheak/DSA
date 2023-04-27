fd69325a4d53b5f34df79e54fe9373e5a643d4a3
theak <theak@theak.theak>
2023-04-27 17:36:51 +0530

    add deleteData function in BST

--- DSA/src/binaryTree/BST.java
+++ DSA/src/binaryTree/BST.java
@@ -4,38 +4,126 @@ public class BST {
 	private BinaryTreeNode<Integer> root ;
 	private int size;
 	
-	private boolean helper(BinaryTreeNode<Integer> root, int x) {
-		if(root==null) {
+	private static boolean isPresenthelper(BinaryTreeNode<Integer> node, int x) {
+		if(node==null) {
 			return false;
 		}
-		if(root.data == x) {
+		if(node.data == x) {
 			return true;
 		}
-		if(x<root.data) {
-			return helper(root.left, x);
+		if(x<node.data) {
+			return isPresenthelper(node.left, x);
 		}else {
-			return helper(root.right, x);
+			return isPresenthelper(node.right, x);
 		}
 	}
+	
 	
 	public boolean isPresent(int x) {
 		
-		return helper(root, x);
+		return isPresenthelper(root, x);
+	}
+	
+	private BinaryTreeNode<Integer> insertHelper(BinaryTreeNode<Integer> node, int x){
+		if(node == null) {
+			BinaryTreeNode<Integer> child = new BinaryTreeNode<Integer>(x);
+			size++;
+			return child;
+		}
+		if(x<node.data) {
+			node.left = insertHelper(node.left, x);
+		}else {
+			node.right = insertHelper(node.right, x);
+		}
+		return node;
 	}
 	
 	public void insert(int x) {
+		root = insertHelper(root, x);
 		
 	}
+	
+	private static int smallestNode(BinaryTreeNode<Integer> node) {
+		if(node==null) {
+			return Integer.MAX_VALUE;
+		}
+		return Math.min(node.data, Math.min(smallestNode(node.left), smallestNode(node.right)));
+	}
+	
+	private BSTDeleteReturn deleteDataHelper(BinaryTreeNode<Integer> node, int x) {
+		if(node == null) {
+			return new BSTDeleteReturn(null, false);
+		}
+		
+		if(node.data < x) {
+			BSTDeleteReturn outputRight  = deleteDataHelper(node.right, x);
+			node.right = outputRight.root; 
+			outputRight.root = node;
+			return outputRight;
+		}
+		if(node.data > x) {
+			BSTDeleteReturn outputLeft  = deleteDataHelper(node.left, x);
+			node.left = outputLeft.root; 
+			outputLeft.root = node;
+			return outputLeft;
+		}
+//		0 children
+		if(node.left ==null && node.right == null) {
+			return new  BSTDeleteReturn(null, true);
+		}
+//		only left child
+		if(node.left != null && node.right == null) {
+			return new BSTDeleteReturn(node.left, true);
+		}
+//		only right child
+		if(node.right != null && node.left == null) {
+			return new BSTDeleteReturn(node.right, true);
+		}
+		
+//		both children are present
+		int rightMin = smallestNode(node.right);
+		node.data = rightMin;
+		BSTDeleteReturn output = deleteDataHelper(node.right, rightMin);
+		node.right = output.root;
+		return new BSTDeleteReturn(node, true);	
+	}
+	
 	public  boolean deleteData(int x) {
-		return false;
+		BSTDeleteReturn output = deleteDataHelper(root, x);
+		if(output.deleted) {
+			size--;
+		}
+		return output.deleted;
 	}
 	
 	public int size() {
 		return size;
 	}
 	
-	public void printTree() {
+	private static void printTreeHelper(BinaryTreeNode<Integer> node) {
+		if(node == null) {
+			return;
+		}
+		System.out.print(node.data + " : ");
+		if(node.left!=null) {
+			System.out.print("L" + node.left.data);
+		}
+		if(node.right!=null) {
+			if(node.left!=null) {
+				System.out.print(", R" + node.right.data);
+			}else {
+				System.out.print("R" +  node.right.data);
+			}
+		}
+		System.out.println();
+		printTreeHelper(node.left);
+		printTreeHelper(node.right);
+		
 		
 	}
-
+	
+	public void printTree() {
+		printTreeHelper(root);
+		
+	}
 }
--- DSA/src/binaryTree/BSTUse.java
+++ DSA/src/binaryTree/BSTUse.java
@@ -3,6 +3,19 @@ package binaryTree;
 public class BSTUse {
 
 	public static void main(String[] args) {
+		BST b = new BST();
+		b.insert(4);
+		b.insert(3);
+		b.insert(2);
+		b.insert(1);
+		b.insert(5);
+		b.insert(9);
+		b.insert(10);
+		
+		
+		b.printTree();
+		System.out.println(b.deleteData(4));
+		b.printTree();
 		
 
 	}
