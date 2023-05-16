c8c5393ea0475c5a418d9ab513c94b18b3a58402
theak <theak@theak.theak>
2023-05-16 17:59:58 +0530

    add dp knapsack iterative

--- DSA/src/dynammingProgramming/KnapSack.java
+++ DSA/src/dynammingProgramming/KnapSack.java
@@ -17,12 +17,32 @@ public class KnapSack {
 		}
 		return ans;
 	}
+	
+	public static int knapSackDI(int W, int[] val, int[] wt) {
+		int n = val.length;
+		int[][] dp = new int[n+1][W+1];
+		
+		for(int i=n-1; i>=0; i--) {
+			for(int w=0; w<=W; w++) {
+				int ans;
+				if(wt[i]<=w) {
+					int ans1 = val[i] + dp[i+1][w-wt[i]];
+					int ans2 = dp[i+1][w];
+					ans = Math.max(ans1, ans2);
+				}else {
+					ans = dp[i+1][w];
+				}
+				dp[i][w] = ans;
+			}
+		}
+		return dp[0][W];
+	}
 
 	public static void main(String[] args) {
 		int val[] = {200, 300, 100};
 		int wt[] = {20, 25, 30};
 		int w = 50;
-		int ans = knapSack(val, wt, w, 0);
+		int ans = knapSackDI(w, val, wt);
 		System.out.println(ans);
 		
 
